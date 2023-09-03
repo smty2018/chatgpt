0581d7b70c197cb68a6944dcb6a2d1301d4d0b07
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-09-03 16:43:18 +0530

    Initial commit

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/constants/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = Color.fromARGB(255, 255, 255, 255);
+Color cardColor = Color.fromARGB(255, 239, 184, 245);
